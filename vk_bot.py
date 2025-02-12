32e1e84438c3e0e3ea97d675aac5ea1be6d019de
Shishkin PRO <viktorshish@gmail.com>
2025-02-12 16:42:41 +0300

    Add VK bot

---
+++ vk_bot.py
@@ -0,0 +1,27 @@
+from environs import Env
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+
+def main():
+    env = Env()
+    env.read_env()
+
+    vk_token = env.str('VK_TOKEN')
+
+    vk_session = vk_api.VkApi(token=vk_token)
+
+    longpoll = VkLongPoll(vk_session)
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW:
+            print('Новое сообщение:')
+            if event.to_me:
+                print('Для меня от: ', event.user_id)
+            else:
+                print('От меня для: ', event.user_id)
+            print('Текст:', event.text)
+
+
+if __name__ == '__main__':
+    main()
