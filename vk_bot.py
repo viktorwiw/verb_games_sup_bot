77884cd3d4b9d69fd1797c0cbd0780c915090538
Shishkin PRO <viktorshish@gmail.com>
2025-02-12 20:30:30 +0300

    Ignore if user's messages are not understood

--- vk_bot.py
+++ vk_bot.py
@@ -22,9 +22,13 @@ def detect_intent_texts(project_id, session_id, text, language_code) -> str:
     text_input = dialogflow.TextInput(text=text, language_code=language_code)
     query_input = dialogflow.QueryInput(text=text_input)
 
+
     response = session_client.detect_intent(
         request={"session": session, "query_input": query_input}
         )
+    if response.query_result.intent.is_fallback:
+        return
+
     return response.query_result.fulfillment_text
 
 
