20e5d59e6741b3333a31748bba6037658deb3d6f
Shishkin PRO <viktorshish@gmail.com>
2025-02-22 22:55:11 +0300

    rename functions and add logging

--- create_intent.py
+++ create_intent.py
@@ -1,16 +1,17 @@
 import json
 import logging
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
 
 from environs import Env
 from google.api_core.exceptions import InvalidArgument, GoogleAPIError
 from google.cloud import dialogflow
 
 
-logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def open_json():
+def get_questions():
     with open('questions.json', 'r', encoding='utf-8') as file:
         questions = json.load(file)
     return questions
@@ -28,11 +29,11 @@ def create_intent(
     training_phrases = []
     for training_phrases_part in training_phrases_parts:
         part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
-        training_phrase = dialogflow.Intent.TrainingPhrase([part])
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
         training_phrases.append(training_phrase)
 
     text = dialogflow.Intent.Message.Text(text=message_texts)
-    message = dialogflow.Intent.Message(text)
+    message = dialogflow.Intent.Message(text=text)
 
     intent = dialogflow.Intent(
         display_name=display_name,
@@ -48,12 +49,22 @@ def create_intent(
 
 
 def main():
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+            "%(name)s | %(levelname)s | %(asctime)s\n"
+        "%(message)s | %(filename)s:%(lineno)d",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    handler = RotatingFileHandler(Path(__file__).parent / 'create_intent.log', maxBytes=1000, backupCount=2)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
     env = Env()
     env.read_env()
 
     project_id = env.str("PROJECT_ID")
 
-    for display_name, intent in open_json().items():
+    for display_name, intent in get_questions().items():
         training_phrases_parts = intent['questions']
         message_texts = [intent['answer']]
         try:
